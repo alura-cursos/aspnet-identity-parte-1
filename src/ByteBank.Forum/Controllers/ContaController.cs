b8aa975eed68da6afae1e1163e161214682e125a
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-03-06 08:10:22 -0300

    termino da aula-02

--- src/ByteBank.Forum/Controllers/ContaController.cs
+++ src/ByteBank.Forum/Controllers/ContaController.cs
@@ -1,9 +1,11 @@
 ﻿using ByteBank.Forum.Models;
 using ByteBank.Forum.ViewModels;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -17,8 +19,27 @@ namespace ByteBank.Forum.Controllers
         }
 
         [HttpPost]
-        public ActionResult Registrar(ContaRegistrarViewModel modelo)
+        public async Task<ActionResult> Registrar(ContaRegistrarViewModel modelo)
         {
+            if(ModelState.IsValid)
+            {
+                var dbContext = new IdentityDbContext<UsuarioAplicacao>("DefaultConnection");
+                var userStore = new UserStore<UsuarioAplicacao>(dbContext);
+                var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                var novoUsuario = new UsuarioAplicacao();
+
+                novoUsuario.Email = modelo.Email;
+                novoUsuario.UserName = modelo.UserName;
+                novoUsuario.NomeCompleto = modelo.NomeCompleto;
+
+                await userManager.CreateAsync(novoUsuario, modelo.Senha);
+                
+                // Podemos incluir o usuário
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Alguma coisa de errado aconteceu!
             return View(modelo);
         }
     }
---
+++ src/ByteBank.Forum/Models/UsuarioAplicacao.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ByteBank.Forum.Models
+{
+    public class UsuarioAplicacao : IdentityUser
+    {
+        public string NomeCompleto { get; set; }
+    }
+}
